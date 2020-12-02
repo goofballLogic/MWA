2f179da0e1fb2cdfbc279d76eb4b0b980832a604
Andrew Gibson <agibson@hublsoft.com>
2020-12-02 14:15:32 +0000

    important change

--- MWA/Startup.cs
+++ MWA/Startup.cs
@@ -15,7 +15,7 @@ namespace MWA
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello James!");
                 });
             });
         }
